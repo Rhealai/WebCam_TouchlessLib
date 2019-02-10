c6fafadcce186e372bd2c56ce0290cc8389b4fff
Rhealai <44374177+Rhealai@users.noreply.github.com>
2019-02-11 02:32:15 +0800

    20190211

--- WebCam/Form1.Designer.vb
+++ WebCam/Form1.Designer.vb
@@ -26,12 +26,18 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(154, 11)
+        Me.Button1.Location = New System.Drawing.Point(19, 11)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(121, 40)
         Me.Button1.TabIndex = 0
@@ -40,21 +46,82 @@ Partial Class Form1
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(6, 56)
+        Me.PictureBox1.Location = New System.Drawing.Point(19, 57)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(421, 247)
+        Me.PictureBox1.Size = New System.Drawing.Size(640, 480)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
         'Timer1
         '
-        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 40
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(269, 11)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 40)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(455, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(81, 37)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "2048x1536"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(153, 11)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(110, 38)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(375, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(74, 35)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "1024x768"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(542, 12)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(90, 35)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "640x480"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(635, 14)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(55, 32)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(435, 306)
+        Me.ClientSize = New System.Drawing.Size(692, 566)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
@@ -66,4 +133,10 @@ Partial Class Form1
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
 End Class
--- WebCam/Form1.vb
+++ WebCam/Form1.vb
@@ -1,19 +1,112 @@
 ﻿Imports TouchlessLib
+Imports System.Drawing.Imaging
+
 Public Class Form1
-    Dim pic As New Drawing.Bitmap(500, 400)
+    'Dim pic As New Drawing.Bitmap(640, 480)
     Dim Touchless As New TouchlessLib.TouchlessMgr
+    'Dim Marker As New TouchlessLib.Marker
     Dim camera1 As TouchlessLib.Camera = Touchless.Cameras.ElementAt(0)
+    Dim EA As TouchlessLib.CameraEventArgs
+
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        PictureBox1.Image = camera1.GetCurrentImage
+        'PictureBox1.Image = camera1.GetCurrentImage
+        Timer1.Enabled = True
     End Sub
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Touchless.CurrentCamera = camera1
-        Touchless.CurrentCamera.CaptureHeight = 200
-        Touchless.CurrentCamera.CaptureWidth = 300
+        'Touchless.CurrentCamera.CaptureHeight = 480
+        'Touchless.CurrentCamera.CaptureWidth = 640
+        camera1.FpsLimit = 25
+        camera1.CaptureHeight = 480
+        camera1.CaptureWidth = 640
+        camera1.RotateFlip = RotateFlipType.RotateNoneFlipXY
+
     End Sub
 
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles Timer1.Tick
         PictureBox1.Image = camera1.GetCurrentImage
+
+        'PictureBox1.Image = Touchless.CurrentCamera.GetCurrentImage()
+
+        Dim bt As Bitmap = Touchless.CurrentCamera.GetCurrentImage()
+
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+
+        'Dim t As Single = 0.6F
+
+        'Dim ptsArray()() As Single = {
+        'New Single() {1, 0, 0, 0, 0},
+        'New Single() {0, 1, 0, 0, 0},
+        'New Single() {0, 0, 1, 0, 0},
+        'New Single() {0, 0, 0, t, 0},
+        'New Single() {0, 0, 0, 0, 1}
+        '}
+        'Dim clrMatrix As New ColorMatrix(ptsArray)
+        'Dim imgAttributes As New ImageAttributes()
+        'imgAttributes.SetColorMatrix(clrMatrix,
+        '                             ColorMatrixFlag.Default,
+        '                             ColorAdjustType.Bitmap)
+
+
+        'g.DrawImage(bt, New Rectangle(0, 0, bt.Size.Width, bt.Size.Height),
+        '           0, 0, bt.Size.Width, bt.Size.Height, GraphicsUnit.Pixel, imgAttributes)
+
+        g.FillRectangle(Brushes.Red, 30, 40, 10, 10)
+        g.DrawLine(Pens.Red, 60, 60, 80, 80)
+
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        camera1.ShowPropertiesDialog(Me.Handle)
+        'MsgBox(camera1.ToString())
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        'MsgBox(Me.Touchless.Cameras.Count)
+        camera1.CaptureHeight = 1536
+        camera1.CaptureWidth = 2048
+        Me.Size = New System.Drawing.Size(camera1.CaptureWidth + 60, camera1.CaptureHeight + 120)
+    End Sub
+
+    Private Sub CurrentCamera_OnImaheCaptured(ByVal sender As System.Object, ByVal e As CameraEventArgs)
+        Me.PictureBox1.Image = e.Image
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        camera1.Dispose()
+    End Sub
+
+    Private Sub Form1_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Resize
+        camera1.CaptureHeight = Me.Size.Height - 120
+        camera1.CaptureWidth = Me.Size.Width - 60
+
+        PictureBox1.Size = New System.Drawing.Size(camera1.CaptureWidth, camera1.CaptureHeight)
+        PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        camera1.CaptureHeight = 768
+        camera1.CaptureWidth = 1024
+        Me.Size = New System.Drawing.Size(camera1.CaptureWidth + 60, camera1.CaptureHeight + 120)
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        camera1.CaptureHeight = 480
+        camera1.CaptureWidth = 640
+        Me.Size = New System.Drawing.Size(camera1.CaptureWidth + 60, camera1.CaptureHeight + 120)
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        g.FillRectangle(Brushes.Red, 30, 40, 1, 1)
+        g.DrawLine(Pens.Red, 60, 60, 80, 80)
+
     End Sub
 End Class
